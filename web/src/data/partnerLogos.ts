c8d2ae06afc02d4105067bab77927735b4a3f0cf
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-03-23 10:41:34 +0100

    made partner logo and testimonials sections

---
+++ web/src/data/partnerLogos.ts
@@ -0,0 +1,14 @@
+import logo1 from "../assets/images/partner-logos/Forskerforbundetny.png";
+import logo2 from "../assets/images/partner-logos/Fullstakk.png";
+import logo3 from "../assets/images/partner-logos/Mesterbrevnemnda.png";
+import logo4 from "../assets/images/partner-logos/NBBL.png";
+import logo5 from "../assets/images/partner-logos/NITO.png";
+import logo6 from "../assets/images/partner-logos/Utdanningsforbundet.png";
+import logo7 from "../assets/images/partner-logos/arbmand.png";
+import logo8 from "../assets/images/partner-logos/fellesforbundet.png";
+import logo9 from "../assets/images/partner-logos/fysio.png";
+import logo10 from "../assets/images/partner-logos/norsk-eiendom.png";
+import logo11 from "../assets/images/partner-logos/ytf.png";
+
+
+export const partnerLogos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11];
---
+++ web/src/data/testimonials.ts
@@ -0,0 +1,20 @@
+export const testimonials = [
+  {
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+    author: "Ola Nordmann",
+    jobTitle: "Fullstakk",
+  },
+  {
+    description:
+      "Veldig god oppfølging og en trygg prosess fra start til slutt.",
+    author: "Kari Hansen",
+    jobTitle: "Daglig leder",
+  },
+  {
+    description:
+      "Profesjonell, effektiv og enkel å samarbeide med.",
+    author: "Per Johansen",
+    jobTitle: "HR-sjef",
+  },
+];
