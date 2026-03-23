5fefc08b15b1e46c6685a2bd9e906e66fd7719ed
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-03-23 11:08:55 +0100

    working on testimonial styles

--- web/src/data/testimonials.ts
+++ web/src/data/testimonials.ts
@@ -1,20 +1,20 @@
 export const testimonials = [
   {
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
-    author: "Ola Nordmann",
-    jobTitle: "Fullstakk",
+      "Anne Marit leverte en rekrutteringsprosess av høy kvalitet. Hennes brede nettverk, grundige innsikt og gode rådgivning ga oss et svært godt beslutningsgrunnlag. Den kontinuerlige og forutsigbare oppfølgingen gjorde prosessen både effektiv og trygg.",
+    author: "Tone Tellevik Dahl",
+    jobTitle: "Administrerende direktør - Norsk Eiendom ",
   },
   {
     description:
-      "Veldig god oppfølging og en trygg prosess fra start til slutt.",
-    author: "Kari Hansen",
-    jobTitle: "Daglig leder",
+      "Med Anne Marit fikk vi en effektiv og erfaren ressursperson vi kunne sparre med og få direkte råd fra, samt en sømløs search og intervjuprosess. Som ledergruppe var tiden spart, og beslutningsstøtten når det stod mellom sterke kandidater særs verdifull - og personen vi ansatte er en stjerne i jobben",
+    author: "Markus Ekornes Myhre",
+    jobTitle: "CPO/fagdirektør - Fullstakk",
   },
   {
     description:
-      "Profesjonell, effektiv og enkel å samarbeide med.",
+      "I NITO har vi jobbet med Anne Marit over mange år, og hun har bistått oss i en rekke rekrutteringer. Anne Marit er veldig pålitelig, og følger opp både oppdragsgiver og kandidatene på en veldig god og profesjonell måte gjennom hele rekrutteringsprosessen. Vi kan trygt anbefale hennes bistand.",
     author: "Per Johansen",
-    jobTitle: "HR-sjef",
+    jobTitle: "HR-direktør - NITO",
   },
 ];
