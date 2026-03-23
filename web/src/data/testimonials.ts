d78655f69380e1a5051c7205d5115207f7eca7d9
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-03-23 12:53:18 +0100

    added all testimonials static

--- web/src/data/testimonials.ts
+++ web/src/data/testimonials.ts
@@ -14,7 +14,56 @@ export const testimonials = [
   {
     description:
       "I NITO har vi jobbet med Anne Marit over mange år, og hun har bistått oss i en rekke rekrutteringer. Anne Marit er veldig pålitelig, og følger opp både oppdragsgiver og kandidatene på en veldig god og profesjonell måte gjennom hele rekrutteringsprosessen. Vi kan trygt anbefale hennes bistand.",
-    author: "Per Johansen",
+    author: "Janicke Skatvold",
     jobTitle: "HR-direktør - NITO",
+  },
+   {
+    description:
+      "Jeg vil gjerne gi min beste anbefaling til Anne Marit. Hun har vært en svært dyktig og profesjonell rekrutterer, samtidig som hun har vist ekte engasjement og omtanke i prosessen. Hun var tilgjengelig, tydelig og skapte en trygg ramme rundt hele rekrutteringsløpet. Jeg følte meg godt ivaretatt hele veien, og jeg er veldig takknemlig for samarbeidet. Jeg vil uten tvil anbefale henne videre.",
+    author: "Birgitte Olafsen",
+    jobTitle: "generalsekretær - Forskerforbundet",
+  }, 
+  {
+    description:
+      "Jeg møtte Anne Marit første gang som kandidat til min nåværende stilling. Jeg opplevde å bli godt ivaretatt allerede fra vår første samtale på telefon, og deretter gjennom hele prosessen. Senere har jeg møtt Anne Marit som oppdragsgiver/kunde, og også fra den synsvinkelen opplever jeg Anne Marit både profesjonell, tydelig og som en som evner å forstå kundens behov. ",
+    author: "Hilde Loftesnes Nylén",
+    jobTitle: "Personal- og utviklingsleder - Fellesforbundet",
+  },
+  {
+    description:
+      "Vi NBBL benyttet Anne Marie Heber Nærby i rekruttering av mellomleder. Samarbeidet fungerte utmerket. Hun var effektiv, fleksibel og forsto raskt våre behov. Vårt inntrykk er at hun har god oversikt over kandidater og vurderer deres forskjellige ferdigheter godt.",
+    author: "Bård Folke Fredriksen",
+    jobTitle: "Administrerende direktør - NBBL",
+  },
+  {
+    description:
+      "Jeg har brukt Anne Marit på flere rekrutteringsoppdrag. Jeg vil fortsette å bruke henne også i fremtiden. Anne Marit er grundig, har et stort nettverk og en god metodikk. Jeg vil trekke fram at hun legger mye arbeid i oppdragsforståelsen noe som sikrer et godt resultat både for arbeidsgiver og arbeidstaker.",
+    author: "Frode Jahren",
+    jobTitle: "Generalsekretær - Norsk Fysioterapeutforbund",
+  },
+  /*
+  {
+    description:
+      "Anne Marit har ved flere anledninger bistått oss med å tydeliggjøre stillingsbehov, utforme utlysningstekster og gjennomføre rekrutteringsprosesser i tett samarbeid med oss. Dette har gjeldt sentrale og delvis utradisjonelle stillinger til vår organisasjon der vi i utgangspunktet var usikre på hvilken kompetanse og hvilke personlige egenskaper vi faktisk trengte. Anne Marit har vist svært god innsikt i vår organisasjon og har vært tydelige og faglig solide i sine vurderinger og anbefalinger. Kandidatene som er rekruttert gjennom prosessen har fungert meget godt hos oss. Vi kan trygt anbefale Anne Marit.",
+    author: "Trond Karlsen",
+    jobTitle: "Personalsjef - Norsk Arbeidsmandsforbund",
+  },*/
+  {
+    description:
+      "Anne Marit er en svært dyktig rekrutterer. Hun hadde en meget god evne til å identifisere våre behov og fant frem til svært gode kandidater. Hennes råd og veiledning under rekrutteringsprosessen var verdifull, og dette resulterte i flere vellykkede ansettelser.",
+    author: "Linda Jæger",
+    jobTitle: "Generalsekretær i Yrkestrafikkforbundet",
+  },
+  {
+    description:
+      "Vi opplevde en profesjonell rekrutterer som skapte trygghet i prosessen. Dialogen var god og Anne Marit holdt det hun lovet.",
+    author: "Anne-Dorthe Nodland Aasen",
+    jobTitle: "Seksjonsleder, seksjon for sekretariatsutvikling - Utdanningsforbundet",
+  },
+  {
+    description:
+      "Anne Marit har bistått Mesterbrevnemnda i flere rekrutteringsprosesser og levert svært gode resultater. Hun er grundig, tillitvekkende og har en god forståelse for både organisasjonen og behovene våre. Takket være hennes arbeid har vi funnet riktige kandidater til flere viktige stillinger.",
+    author: "André Kristiansen",
+    jobTitle: "Daglig leder - Mesterbrevnemnda",
   },
 ];
