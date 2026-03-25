bba8668ee646348484557b261cb9f99fb1b502eb
joakimlees <joakimlee_sletten@outlook.com>
2026-03-25 12:40:23 +0100

    working on hero componenet

---
+++ web/src/data/navLinks.ts
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { href: "/", label: "Hjem" },
+  { href: "/rekruttering", label: "Rekruttering" },
+  { href: "/rådgivning", label: "Rådgivning" },
+  { href: "/kundehistorier", label: "Kundehistorier" },
+  { href: "om-meg", label: "Om meg" },
+  { href: "kontakt", label: "Kontakt" },
+  { href: "ledige-stillinger", label: "Ledige stillinger" },
+];
