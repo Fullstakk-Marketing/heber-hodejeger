ff0afe2219668887a9f141bf8581b2b2500b95c7
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-03-26 12:45:20 +0100

    working on split section

---
+++ web/src/data/heroContent.ts
@@ -0,0 +1,18 @@
+export const heroContent = {
+  omMeg: {
+    title: "Om meg",
+    description: "Heber hodejeger drives av meg, Anne Marit Heber Nærby: tidligere politiker, journalist, kommunikasjonsrådgiver og markedsfører – inntil jeg for over 20 år siden begynte med rekruttering. Jeg rekrutterer kun til sektorer og roller jeg kjenner godt! Det har jeg lykkes med i mange år.",
+  },
+  rekruttering: {
+    title: "Rekruttering",
+    description: "Jeg hjelper bedrifter med ...",
+  },
+  rådgivning: {
+    title: "Rådgivning",
+    description: "Sammen finner vi ...",
+  },
+  kontakt: {
+    title: "Kontakt",
+    description: "Ta kontakt for en uforpliktende prat.",
+  },
+} as const;
