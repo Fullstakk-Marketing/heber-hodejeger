41acff5a543a9daf767a6eec2bd8d54eda2df7f5
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-03-26 15:23:09 +0100

    made bannerCard componenet and finsihed about page

--- web/src/data/heroContent.ts
+++ web/src/data/heroContent.ts
@@ -1,18 +1,22 @@
 export const heroContent = {
   omMeg: {
-    title: "Om meg",
+    title: "Om Heber hodejeger",
     description: "Heber hodejeger drives av meg, Anne Marit Heber Nærby: tidligere politiker, journalist, kommunikasjonsrådgiver og markedsfører – inntil jeg for over 20 år siden begynte med rekruttering. Jeg rekrutterer kun til sektorer og roller jeg kjenner godt! Det har jeg lykkes med i mange år.",
+  },
+  referanser: {
+    title: "Referanser",
+    description: "Jeg har vært så heldig å fått hjelpe et bredt spekter av organisasjoner med rekrutteringer av både ledere og spesialister. Noen har jeg bistått over flere år. Jeg håper det betyr at jeg leverer kvalitet. Her kan du lese litt om hva et utvalg mener",
   },
   rekruttering: {
     title: "Rekruttering",
-    description: "Jeg hjelper bedrifter med ...",
+    description: "Rekrutteringsbehov kan variere. La meg gjerne ta hånd om prosessen fra A-Å, eller kun deler av den. Jeg er fleksibel.",
   },
   rådgivning: {
     title: "Rådgivning",
-    description: "Sammen finner vi ...",
+    description: "Forbedre dine sjanser om jobbtilbud og intervjuer! Det kan være vanskelig å skille seg ut i mengden. Jeg hjelper deg med å løfte frem din kompetanse og egenskaper i CV'n og å skrive en god søknad – for nei, søknadsteksten er ikke død. ",
   },
   kontakt: {
     title: "Kontakt",
-    description: "Ta kontakt for en uforpliktende prat.",
+    description: "Det er selvsagt helt uforpliktende å ta kontakt å høre om hva jeg kan bistå deg med. Ser frem til å høre fra deg!",
   },
 } as const;
