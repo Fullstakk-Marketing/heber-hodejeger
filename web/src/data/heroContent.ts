5b2961306f545ceece6f12a81d9f9af1f1540d2e
joakimlees <joakimlee_sletten@outlook.com>
2026-03-27 15:14:25 +0100

    added some content on all pages and rdy to make first test deploy

--- web/src/data/heroContent.ts
+++ web/src/data/heroContent.ts
@@ -1,22 +1,27 @@
 export const heroContent = {
   omMeg: {
     title: "Om Heber hodejeger",
-    description: "Heber hodejeger drives av meg, Anne Marit Heber Nærby: tidligere politiker, journalist, kommunikasjonsrådgiver og markedsfører – inntil jeg for over 20 år siden begynte med rekruttering. Jeg rekrutterer kun til sektorer og roller jeg kjenner godt! Det har jeg lykkes med i mange år.",
+    description:
+      "Heber hodejeger drives av meg, Anne Marit Heber Nærby: tidligere politiker, journalist, kommunikasjonsrådgiver og markedsfører – inntil jeg for over 20 år siden begynte med rekruttering. Jeg rekrutterer kun til sektorer og roller jeg kjenner godt! Det har jeg lykkes med i mange år.",
   },
   referanser: {
     title: "Referanser",
-    description: "Jeg har vært så heldig å fått hjelpe et bredt spekter av organisasjoner med rekrutteringer av både ledere og spesialister. Noen har jeg bistått over flere år. Jeg håper det betyr at jeg leverer kvalitet. Her kan du lese litt om hva et utvalg mener.",
+    description:
+      "Jeg har vært så heldig å fått hjelpe et bredt spekter av organisasjoner med rekrutteringer av både ledere og spesialister. Noen har jeg bistått over flere år. Jeg håper det betyr at jeg leverer kvalitet. Her kan du lese litt om hva et utvalg mener.",
   },
   rekruttering: {
     title: "Rekruttering",
-    description: "Rekrutteringsbehov kan variere. La meg gjerne ta hånd om prosessen fra A-Å, eller kun deler av den. Jeg er fleksibel.",
+    description:
+      "Rekrutteringsbehov kan variere. La meg gjerne ta hånd om prosessen fra A-Å, eller kun deler av den. Jeg er fleksibel.",
   },
   rådgivning: {
     title: "Rådgivning",
-    description: "Forbedre dine sjanser om jobbtilbud og intervjuer! Det kan være vanskelig å skille seg ut i mengden. Jeg hjelper deg med å løfte frem din kompetanse og egenskaper i CV'n og å skrive en god søknad – for nei, søknadsteksten er ikke død. ",
+    description:
+      "Forbedre dine sjanser om jobbtilbud og intervjuer! Det kan være vanskelig å skille seg ut i mengden. Jeg hjelper deg med å løfte frem din kompetanse og egenskaper i CV'n og å skrive en god søknad – for nei, søknadsteksten er ikke død. ",
   },
   kontakt: {
     title: "Kontakt",
-    description: "Det er selvsagt helt uforpliktende å ta kontakt å høre om hva jeg kan bistå deg med. Ser frem til å høre fra deg!",
+    description:
+      "Det er selvsagt helt uforpliktende å ta kontakt å høre om hva jeg kan bistå deg med. Ser frem til å høre fra deg!",
   },
 } as const;
