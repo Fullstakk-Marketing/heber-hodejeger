02226a4904b2f68680e4f0f0b22c1a7ecf2d65f2
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-04-22 12:52:46 +0200

    added sitemap and fixed vualanbilites

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.heberhodejeger.no',
+  integrations: [
+    sitemap({
+      filter: (page) => page !== 'https://www.heberhodejeger.no/takk',
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
