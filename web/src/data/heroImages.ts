4ddcaebeb3be1b44c3f52b3ef8e3cbc0f76a508a
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2026-04-22 10:32:44 +0200

    replaced hero image and did small content changes

--- web/src/data/heroImages.ts
+++ web/src/data/heroImages.ts
@@ -1,6 +1,7 @@
-import hero1 from "../assets/images/images/women1.jpeg";
-import hero2 from "../assets/images/images/man1.jpeg";
-import hero3 from "../assets/images/images/man2.jpeg";
-import hero4 from "../assets/images/images/women2.jpeg";
+import hero1 from "../assets/images/images/am1.jpeg";
+import hero2 from "../assets/images/images/man2.jpeg";
+import hero3 from "../assets/images/images/women1.jpeg";
+import hero4 from "../assets/images/images/man1.jpeg";
+
 
 export const heroImages = [hero1, hero2, hero3, hero4];
